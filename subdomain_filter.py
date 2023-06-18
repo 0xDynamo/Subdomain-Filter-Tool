d8ce1a6366e7240c968ab80a15747b1bbd9b80a0
Jimmy <78177659+0xDynamo@users.noreply.github.com>
2023-06-18 14:36:49 +0200

    Update subdomain_filter.py

--- subdomain_filter.py
+++ subdomain_filter.py
@@ -27,12 +27,23 @@ def filter_subdomains(file_content, root_domain, off_scope_list):
         
     return matches
 
+def extract_ips(file_content):
+    # Match IP addresses using regex
+    pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    matches = re.findall(pattern, ' '.join(file_content))
+    unique_ips = list(set(matches))
+    sorted_ips = sorted(unique_ips)
+    
+    return sorted_ips
+
 def main():
-    parser = argparse.ArgumentParser(description='Filter subdomains from file')
+    parser = argparse.ArgumentParser(description='Filter subdomains from file and extract IP addresses')
     parser.add_argument('-i', metavar='input', type=str, required=True, help='Input file path')
     parser.add_argument('-d', metavar='domain', type=str, required=True, help='Root domain to filter subdomains')
-    parser.add_argument('-o', metavar='output', type=str, help='Output file to write the result')
+    parser.add_argument('-o', metavar='output', type=str, help='Output file to write the filtered subdomains')
     parser.add_argument('-r', '--off-scope', type=str, help='Wildcard or file containing off-scope wildcards')
+    parser.add_argument('-ip', action='store_true', help='Extract IP addresses from the input file')
+    parser.add_argument('-oI', metavar='output_ips', type=str, help='Output file to write the extracted IP addresses')
     args = parser.parse_args()
 
     file_content = read_file(args.i)
@@ -52,9 +63,19 @@ def main():
         with open(args.o, 'w') as output_file:
             for subdomain in subdomains:
                 output_file.write(f'{subdomain}\n')
-        print(f'Output has been written to {args.o}')
+        print(f'Filtered subdomains have been written to {args.o}')
     else:
         print('\n'.join(subdomains))
+
+    if args.ip:
+        ips = extract_ips(file_content)
+        if args.oI:
+            with open(args.oI, 'w') as output_ips_file:
+                for ip in ips:
+                    output_ips_file.write(f'{ip}\n')
+            print(f'Extracted and sorted IP addresses have been written to {args.oI}')
+        else:
+            print('\n'.join(ips))
 
 if __name__ == "__main__":
     main()
